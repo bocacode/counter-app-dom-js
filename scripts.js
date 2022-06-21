d598ee8fe3a999d632b152a25c3bdaf49d3a9c56
jonathans199 <jso1919@gmail.com>
2022-06-20 21:09:49 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const counterEl = document.getElementById('counter')
+const totalCount = document.getElementById('total')
+let count = 0
+
+function increase() {
+  // count = count + 1 
+  count += 1 // this shorthand for the line before
+  counterEl.innerHTML = count
+}
+
+// function that decreases
+function decrease(){
+  // count = count - 1
+  count -= // this shorthand for the line before
+  counterEl.innerText = count
+}
+
+// bonus function that prints total
+function printTotal(){
+  let countStr = count + ' - '
+  totalCount.innerHTML += countStr
+}
